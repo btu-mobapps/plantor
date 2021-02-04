47a69776f4b5df3822965206fc76b2880a628be9
Luka Kldiashvili <42884387+lukakldiashvili@users.noreply.github.com>
2021-02-04 17:06:24 +0400

    added recycler view with glide

--- plantor/app/src/main/java/com/mobapps/plantor/ui/dashboard/DashboardFragment.kt
+++ plantor/app/src/main/java/com/mobapps/plantor/ui/dashboard/DashboardFragment.kt
@@ -4,15 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.mobapps.plantor.R
+import com.mobapps.plantor.data.Plant
 
 class DashboardFragment : Fragment() {
 
     private lateinit var dashboardViewModel: DashboardViewModel
+    private  lateinit var recyclerView: RecyclerView
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -22,10 +24,19 @@ class DashboardFragment : Fragment() {
         dashboardViewModel =
                 ViewModelProvider(this).get(DashboardViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
+        recyclerView = root.findViewById(R.id.recycler_view)
+
+        val plantList = ArrayList<Plant>()
+
+        plantList.add(Plant(imgUrl = "https://cdn.pixabay.com/photo/2016/11/14/04/45/elephant-1822636_960_720.jpg"))
+        plantList.add(Plant(imgUrl = "https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_960_720.jpg"))
+        plantList.add(Plant(imgUrl = "https://cdn.pixabay.com/photo/2015/12/01/20/28/fall-1072821_960_720.jpg"))
+
+        val adapter = PlantAdapter(plantList, root.context)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = GridLayoutManager(root.context, 2)
+
         return root
     }
 }
---
+++ plantor/app/src/main/java/com/mobapps/plantor/ui/dashboard/PlantAdapter.kt
@@ -0,0 +1,36 @@
+package com.mobapps.plantor.ui.dashboard
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.mobapps.plantor.R
+import com.mobapps.plantor.data.Plant
+
+class PlantAdapter(private var plants:List<Plant>, private val context: Context): RecyclerView.Adapter<PlantAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.image_view)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.plant_view, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val curPhoto = plants[position]
+
+        Glide.with(context)
+            .load(curPhoto.imgUrl)
+            .placeholder(R.drawable.ic_launcher_background)
+            .centerCrop()
+            .into(holder.imageView)
+    }
+
+    override fun getItemCount(): Int  = plants.size
+
+}
