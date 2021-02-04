9ca04e39d74ad000cb44775055edf4c00c26d433
Luka Kldiashvili <42884387+lukakldiashvili@users.noreply.github.com>
2021-02-04 20:43:20 +0400

    Update Plant.kt

--- plantor/app/src/main/java/com/mobapps/plantor/data/Plant.kt
+++ plantor/app/src/main/java/com/mobapps/plantor/data/Plant.kt
@@ -1,9 +1,9 @@
 package com.mobapps.plantor.data
 
 data class Plant (
-    var name: String? = "",
-    var imgUri: String? = "",
-    var lastWaterDate: String? = "",
-    var waterHour: String? = "",
-    var waterDays: Int? = 0
+    var name: String = "",
+    var imgUri: String = "",
+    var lastWaterDate: String = "",
+    var waterHour: String = "",
+    var waterDays: Int = 0
 )
